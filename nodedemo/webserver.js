3ee4ed05451ddf137e16772e5c75870df321d4a6
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2017-09-09 16:59:53 -0300

    Update webserver.js

--- nodedemo/webserver.js
+++ nodedemo/webserver.js
@@ -1,57 +1,107 @@
-
 const http = require('http')
+const porta = 3000
 
-var tickets = [
-    {codigo:123, titulo: "Duvidas x", "descricao": "Lorem ipsum dolores x"},
-    {codigo:234, titulo: "Duvidas y", "descricao": "Lorem ipsum dolores y"}
-]
 
-http.createServer(function(req,res) {
+const tickets = []
+
+tickets.push({ 
+    codigo: 123,
+    nome: "Fàbio ROgério SJ", 
+    email: "fabio.rogerio.sj@gmail.com"
+})
+
+http.createServer(listenerWebServer).listen(porta)
+console.log(`WebServer rodando na porta ${porta} (Ctrl+C)`)
+
+function listenerWebServer(req, res){
+    
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8' })
+
+    var post = function(index){
+        var body = []
+        return req.on('data', (data) => {
+            body.push(data)
+        }).on('end', () => {
+            body = Buffer.concat(body).toString()
+            var ticket = JSON.parse(body)
+            ticket.codigo = new Date().getTime()
+            if(index < 0 || index == null){
+                tickets.push(ticket)
+            } else {
+                tickets[index].nome = ticket.nome
+                tickets[index].email = ticket.email
+            }
+            return res.end( JSON.stringify(ticket) )
+        })
+    }
     
     if(req.method === "GET" && req.url === "/") {
-        return recursoIndex(req, res)
+        return res.end('{ "message": "API REST!" }')
     }
 
-    if(req.method === "GET" && req.url === "/tickets") {
-        return recursoTickets(req, res)
+    if(req.method === "GET" && req.url.indexOf("/tickets") >= 0) {
+        var split = req.url.split("/");
+        if(split.length === 2) {
+            return res.end( JSON.stringify(tickets) )
+        }        
+        if(split.length === 3){
+            var codigo = split[2]
+            var ticket = {}
+            for(i in tickets) {
+                if(tickets[i].codigo == codigo) {
+                    ticket = tickets[i]
+                }
+            }
+            return res.end( JSON.stringify(ticket) )
+        }
+    }
+
+    if(req.method === "DELETE" && req.url.indexOf("/tickets") >= 0) {
+        var split = req.url.split("/");
+        if(split.length === 2) {
+            return res.end('{}')
+        }        
+        if(split.length === 3){
+            var codigo = split[2]
+            var index = -1
+            for(i in tickets) {
+                if(tickets[i].codigo == codigo) {
+                    index = i
+                }
+            }
+            if(index < 0) {
+                res.writeHead(404)
+                return res.end('Não encontrado!')
+            }
+            tickets.splice(index, 1)
+            return res.end( JSON.stringify(tickets) )
+        }
+    }
+
+    if(req.method === "PUT" && req.url.indexOf("/tickets") >= 0) {
+
+        
+        var split = req.url.split("/");
+        if(split.length === 2) {
+            return post()
+        }        
+        if(split.length === 3){
+            var codigo = split[2]
+            var index = -1
+            for(i in tickets) {
+                if(tickets[i].codigo == codigo) {
+                    index = i
+                }
+            }
+            return post(index)
+        }
     }
 
     if(req.method === "POST" && req.url === "/tickets") {
-        return recursoTicketsNovo(req, res)
+        return post()
     }
 
     res.writeHead(404)
-    res.end("Recurso não encontrado!")
+    res.end('Não encontrado!')
 
-
-}).listen(3000)
-
-function recursoIndex(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
-    return res.end('{"nome": "Fábio","email": "fabio.rogrio.sj@gmail.com"}')
 }
-
-function recursoTickets(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
-    return res.end( JSON.stringify(tickets) )
-}
-
-function recursoTicketsNovo(req, res) {
-    var body = []
-    req.on('data', function(data){
-        body.push(data)
-    }).on('end', () => {
-        body = Buffer.concat(body).toString()
-        var ticket = JSON.parse(body)
-        ticket.codigo = new Date().getTime()
-        tickets.push(ticket)
-        
-        res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
-        return res.end( JSON.stringify(ticket) )
-    })
-}
-
-console.log("Servidor HTTP rodando na porta 3000")
-
-
-
